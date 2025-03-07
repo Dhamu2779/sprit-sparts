e06cd92fd6f43007feee8a9957b5e107689e51d6
Dhamu2779 <145636572+Dhamu2779@users.noreply.github.com>
2025-03-07 13:53:07 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: "I'm Scared",
+    artist: "Anirudh Ravichander",
+    img: "https://static.moviecrow.com/marquee/leo-update-month-begins-with-new-poster-release/221017_thumb_665.jpg",
+    src: "https://pagalnew.com/128-download/44226"
+  },
+  {
+    name: "Sawadeeka",
+    artist: "Anirudh Ravichander, Anthony Daasan",
+    img: "https://pagalnew.com/coverimages/sawadeeka-vidaamuyarchi-500-500.jpg",
+    src: "https://pagalnew.com/128-download/50085"
+  },
+  {
+    name: "Achacho",
+    artist: " Hiphop Tamizha, Kharesma Ravichandran, Srinisha Jayaseelan",
+    img: "https://pagalnew.com/coverimages/Achacho-Aranmanai-4-500-500.jpg",
+    src: "https://pagalnew.com/128-download/46499"
+  },
+  {
+    name: "Sithira Puthiri",
+    artist: "Sai Abhyankkar",
+    img: "https://pagalnew.com/coverimages/sithira-puthiri-sai-abhyankkar-500-500.jpg",
+    src: "https://pagalnew.com/128-download/50366"
+  },
+  {
+    name: "Hey Minnale",
+    artist: "Haricharan, Shweta Mohan",
+    img: "https://pagalnew.com/coverimages/hey-minnale-amaran-tamil-500-500.jpg",
+    src: "https://pagalnew.com/128-download/49162"
+  },
+  {
+    name: "Matta",
+    artist: "Yuvan Shankar Raja, Shenbagaraj Ganesalingam",
+    img: "https://pagalnew.com/coverimages/matta-the-greatest-of-all-time-500-500.jpg",
+    src: "https://pagalnew.com/128-download/49157"
+  },
+  {
+    name: "Maatikkinaaru Orutharu",
+    artist: "Leon James",
+    img: "https://pagalnew.com/coverimages/maatikkinaaru-orutharu-dragon-500-500.jpg",
+    src: "https://pagalnew.com/128-download/50362"
+  },
+  {
+    name: "Paththavaikkum",
+    artist: "Anirudh Ravichander, Deepthi Suresh, Vignesh Shivan",
+    img: "https://pagalnew.com/coverimages/paththavaikkum-devara-part-1-500-500.jpg",
+    src: "https://pagalnew.com/128-download/48249"
+  },
+  {
+    name: "Aasa Kooda",
+    artist: "Sai Abhyankkar, Sai Smriti",
+    img: "https://pagalnew.com/coverimages/aasa-kooda-sai-abhyankkar-500-500.jpg",
+    src: "https://pagalnew.com/128-download/47122"
+  },
+  {
+    name: "Kissik",
+    artist: "Sublahshini",
+    img: "https://pagalnew.com/coverimages/kissik-pushpa-2-tamil-500-500.jpg",
+    src: "https://pagalnew.com/128-download/49488"
+  }
+];
